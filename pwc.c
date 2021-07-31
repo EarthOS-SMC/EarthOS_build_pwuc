0c94dc97348c055660bcdc709067c61952bbf47a
adazem009 <68537469+adazem009@users.noreply.github.com>
2021-07-31 12:17:41 +0200

    Add getklog function

--- pwc.c
+++ pwc.c
@@ -1888,6 +1888,15 @@ int main(int argc, char *argv[])
 				_error("Number of inputs in the second argument must be 1",true,line+1,13,filename);
 			fprintf(ow,"44\n2\n2\n%s\n%s\n1\n%s\n",_getinput(0,0,i,cmd_argc,raw),_getinput(0,1,i,cmd_argc,raw),_getinput(1,0,i,cmd_argc,raw));
 		}
+		else if(strcmp(cmd,"getklog") == 0)
+		{
+			// getklog/output_list_name
+			if(cmd_argc != 1)
+				_error("Number of arguments must be 1",true,line+1,12,filename);
+			if(_getinputc(0,i,cmd_argc,raw) != 1)
+				_error("Number of inputs in the first argument must be 1",true,line+1,13,filename);
+			fprintf(ow,"45\n1\n1\n%s\n",_getinput(0,0,i,cmd_argc,raw));
+		}
 		else
 		{
 			sprintf(err,"Function '%s' not found",cmd);
